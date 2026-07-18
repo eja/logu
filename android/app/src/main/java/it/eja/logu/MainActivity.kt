71afef3fbc2b55a31e150217996f57d6e6b72ffd
Ubaldo Porcheddu <ubaldo@eja.it>
2026-07-18 16:25:53 +0100

    android draft

---
+++ android/app/src/main/java/it/eja/logu/MainActivity.kt
@@ -0,0 +1,34 @@
+// Copyright (C) by Ubaldo Porcheddu <ubaldo@eja.it>
+
+package it.eja.logu
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.WindowManager
+import android.webkit.WebView
+import androidx.webkit.WebViewAssetLoader
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        val assetLoader = WebViewAssetLoader.Builder()
+            .addPathHandler("/", WebViewAssetLoader.AssetsPathHandler(this))
+            .build()
+
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        //webView.settings.domStorageEnabled = true
+
+        webView.webViewClient = MyWebViewClient(assetLoader)
+        webView.loadUrl("https://appassets.androidplatform.net/index.html")
+
+        setContentView(webView)
+    }
+}
+
+class MyWebViewClient(private val assetLoader: WebViewAssetLoader) : android.webkit.WebViewClient() {
+    override fun shouldInterceptRequest(view: WebView, request: android.webkit.WebResourceRequest) =
+        assetLoader.shouldInterceptRequest(request.url)
+}
